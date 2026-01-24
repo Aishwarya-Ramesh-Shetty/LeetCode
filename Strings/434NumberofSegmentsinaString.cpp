4141d1674d4a5ef4029e6fad1d68f2d5c8882af1
Aishwarya-Ramesh-Shetty <shettyaishu050@gmail.com>
2026-01-24 23:09:45 +0530

    Leetcode String Problem-Day 16

---
+++ Strings/434NumberofSegmentsinaString.cpp
@@ -0,0 +1,11 @@
+int countSegments(string s) {
+    int count = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] != ' ' && (i == 0 || s[i - 1] == ' ')) {
+            count++;
+        }
+    }
+
+    return count;
+}
