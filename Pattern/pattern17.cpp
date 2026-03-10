4ac9cf8364a179132887ed61142c38b68b8a2f1a
Aishwarya-Ramesh-Shetty <shettyaishu050@gmail.com>
2026-03-10 06:39:04 +0530

    Solved Palindromic Alphabet Pyramid or Alphabet Hill

--- Pattern/pattern17.cpp
+++ Pattern/pattern17.cpp
@@ -1,17 +1,23 @@
 #include<iostream>
 using namespace std;
 
+
 int main(){
     int n;
-    cout<<"Enter the value of n: ";
+    cout<<"Enter the number of rows: ";
     cin>>n;
 
-
-     for(int i=1;i<=n;i++){
-        for(int j=0;j<i;j++){
-            cout<<(char)('A'+(i-1))<<" ";
+    for(int i=1;i<=n;i++){
+        for(int k=1;k<=n-i;k++){
+            cout<<" ";
         }
-       
+        for(int j=0;j<i;j++){
+            cout<<(char)('A'+j);
+        }
+        for(int m=i-1;m>=1;m--){
+            cout<<(char)('A'+(m-1));
+        }
         cout<<endl;
     }
+
 }
