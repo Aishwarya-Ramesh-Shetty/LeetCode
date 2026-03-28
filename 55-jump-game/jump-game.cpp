9483b4060418e0b13c534134626f309735d1ea44
Aishwarya Shetty <shettyaishu050@gmail.com>
2026-03-28 07:04:41 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (81.99%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int maxReach = 0;
+        if(nums.size() == 1){
+            return true;
+        }
         for(int i=0;i<nums.size();i++){
             if(i>maxReach){
                 return false;
