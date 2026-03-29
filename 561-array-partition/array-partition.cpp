ee5ee404b5754854c7c098ccfe5ac8abb67d1cf6
Aishwarya Shetty <shettyaishu050@gmail.com>
2026-03-29 12:22:51 +0530

    Time: 20 ms (8.04%) | Memory: 32 MB (56.27%) - LeetSync

--- 561-array-partition/array-partition.cpp
+++ 561-array-partition/array-partition.cpp
@@ -4,7 +4,7 @@ public:
         int profit = 0;
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size()-1;i+=2){
-            profit = profit + min(nums[i],nums[i+1]);
+            profit = profit + nums[i];
         }
         return profit;
     }
